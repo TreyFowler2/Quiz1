907caa338276e40d4b2a4b655976c4b8d620f403
Trey <Trey_Fowler2@baylor.edu>
2022-09-20 12:14:36 -0500

    Fixed gitignore

--- createMarketingList.py
+++ createMarketingList.py
@@ -12,6 +12,7 @@
 # 'Carmela Kaubisch': {'email': 'ckaubischdv@wikia.com', 'phone': '307-726-6526'}}
 
 
+
 # Using the dictionary, write the contents to a csv file. The output file shoud be exactly as
 # what is shown in the file - marketinglist.csv.
 # Name your file - marketinglistFINAL.csv
@@ -24,23 +25,38 @@
 import csv
 
 # open the vendorlist file
-
+infile = open('VendorList.csv', 'r')
 
 # create a csv object from the file object
-
+csvfile = csv.reader(infile,delimiter=',')
 
 # create an output file
-
-
-
-
+outfile = open('marketinglistFINAL.csv','w',newline='')
+next(csvfile)
+#writer = csv.writer(outfile)
+#header = ["Name", "Email", "Phone"]
+#writer.writerow(header)
 
 # create an empty dictionary
-
-
+customerinfo = dict()
 
 # iterate through the csv object
+count = 1
+for record in csvfile:
+    customerinfokey = [record[1] + ' ' + record[2]]
+    for number in customerinfokey:
+        customerinfo[number] = {'email': record[4], 'phone': record[5]}
+    #customerinfoemail = [record[4]]
 
+    #customerinfophone = [record[5]]
+    #customerinfo = [customerinfokey]
+    #customervalue = [customerinfoemail, customerinfophone]
+
+
+    #key = record[1] + ' ' + record[2]
+    #customervalue = dict()
+    #customervalue = [record[4], record[5]]
+    #customerinfo = customerinfo[key]
 
 
 
@@ -53,8 +69,8 @@ import csv
 
 
 # iternate through the dictionary and write to the output file
-
+outfile.write(str(customerinfo))
 
 
 # close your output file
-
+outfile.close()
